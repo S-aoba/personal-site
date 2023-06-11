af70e55079b1e62f9fcae8d48081c815add174ce
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-06-11 20:47:19 +0900

    🔥 rm:About Page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,10 +11,10 @@ export const Header = () => {
       </span>
       <nav className='flex grow items-center justify-end gap-1 font-mono text-xs md:gap-3'>
         <Theme />
-        <Link href={'/about'} className='p-2 hover:bg-gray-300 hover:bg-opacity-40 dark:text-white dark:opacity-75'>
-          About
-        </Link>
-        <Link href={'https://twitter.com/aoba__S'} className='flex items-center gap-x-1 p-2 hover:bg-gray-300 hover:bg-opacity-40'>
+        <Link
+          href={'https://twitter.com/aoba__S'}
+          className='flex items-center gap-x-1 p-2 hover:bg-gray-300 hover:bg-opacity-40'
+        >
           <IconBrandTwitter className='h-4 w-4 fill-blue-600 stroke-blue-600' />
           <span className='dark:text-white dark:opacity-75'>Follow me</span>
         </Link>
